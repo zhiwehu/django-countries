93a2cbc32944155e1c81da4bd39b8dbd873bc039
Jeffrey <zhiwehu@gmail.com>
2013-07-11 09:32:38 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='django-countries',
+      version='0.1',
+      description='django-countries',
+      author='Jeffrey Hu',
+      author_email='zhiwehu@gmail.com',
+      url='https://github.com/zhiwehu/django-countries',
+      packages=['countries',],
+      #test_suite='tests',
+      #tests_require=['MiniMock']
+)
